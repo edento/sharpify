5c3dc8b0731eca73ed77be40ea102999161a7869
Eden <eden@edens-MacBook-Pro.local>
2017-04-27 23:47:24 +0300

    Enable/disable plugin toggle with reactive app icon

--- main.js
+++ main.js
@@ -1,59 +1,71 @@
     (function() {
-
+        var storage = chrome.storage.local;
         var url = document.URL;
         var selector;
+        var run = function () {
+            if (url.indexOf('tabs.ultimate-guitar') != -1) {
+                selector = '.js-tab-content >span';
+            } else if (url.indexOf('tab4u.com/tabs') != -1) {
+                selector = '.chords span';
+            } else {
+                return;
+            }
 
-        if (url.indexOf('tabs.ultimate-guitar') != -1) {
-            selector = '.js-tab-content >span';
-        } else if (url.indexOf('tab4u.com/tabs') != -1) {
-            selector = '.chords span';
-        } else {
-            return;
-        }
+            var replaced = 0;
+            var spans = document.querySelectorAll(selector);
+            var CHORDS = {
+                'Ab': 'G#',
+                'Bb': 'A#',
+                'Db': 'C#',
+                'Eb': 'D#',
+                'Gb': 'F#',
+            };
+            var fixedChords = {};
 
-        var replaced = 0;
-        var spans = document.querySelectorAll(selector);
-        var CHORDS = {
-            'Ab': 'G#',
-            'Bb': 'A#',
-            'Db': 'C#',
-            'Eb': 'D#',
-            'Gb': 'F#',
-        };
-        var fixedChords = {};
+            var saveForTest = function(key, value) {
+                fixedChords[key] = value;
+                replaced++;
+            };
 
-        var saveForTest = function(key, value) {
-            fixedChords[key] = value;
-            replaced++;
-        };
-
-        var fixChords = function() {
-            for (var i = 0; i < spans.length; i++) {
-                var current = spans[i].innerText;
-                if (CHORDS.hasOwnProperty(current)) {
-                    spans[i].innerText = CHORDS[current];
-                    saveForTest(current, CHORDS[current]);
-                } else {
-                    for (var bad in CHORDS) {
-                        if (current.indexOf(bad) === 0) {
-                            var fixed = current.replace(bad, CHORDS[bad]);
-                            spans[i].innerText = fixed;
-                            saveForTest(current, fixed);
+            var fixChords = function() {
+                for (var i = 0; i < spans.length; i++) {
+                    var current = spans[i].innerText;
+                    if (CHORDS.hasOwnProperty(current)) {
+                        spans[i].innerText = CHORDS[current];
+                        saveForTest(current, CHORDS[current]);
+                    } else {
+                        for (var bad in CHORDS) {
+                            if (current.indexOf(bad) === 0) {
+                                var fixed = current.replace(bad, CHORDS[bad]);
+                                spans[i].innerText = fixed;
+                                saveForTest(current, fixed);
+                            }
                         }
                     }
                 }
+                console.log("### Chord Fixer: Replaced " + replaced + " bad chords with good ones!", fixedChords);
+            };
+
+            if (document.readyState === 'complete') {
+                fixChords();
+            } else {
+                var interval = setInterval(function() {
+                    if (document.readyState === 'complete') {
+                        clearInterval(interval);
+                        fixChords();
+                    }
+                }, 100);
             }
-            console.log("### Chord Fixer: Replaced " + replaced + " bad chords with good ones!", fixedChords);
         };
 
-        if (document.readyState === 'complete') {
-            fixChords();
-        } else {
-            var interval = setInterval(function() {
-                if (document.readyState === 'complete') {
-                    clearInterval(interval);
-                    fixChords();
-                }
-            }, 100);
-        }
+        storage.get('enabled',function(result){
+            console.log('enabled', result.enabled);
+            if (result.enabled){
+                run();
+            }
+            else {
+                console.log('script is disabled');
+            }
+        });
+
     })();
--- popup.js
+++ popup.js
@@ -1,7 +1,35 @@
 var toggle = document.getElementById("isPluginOn");
+var storage = chrome.storage.local;
 
-console.log("Found element", toggle)
-
-toggle.addEventListener("change", function(e) {
-    console.log("toggled");
+toggle.addEventListener("change", function (e) {
+    console.log("toggled: ", toggle.checked);
+    save_options();
 });
+
+restore_options();
+
+function updateIcon(enabled) {
+    if (enabled) {
+        chrome.browserAction.setIcon({path: 'img/icon_38.png'});
+    }
+    else {
+        chrome.browserAction.setIcon({path: 'img/icon-disabled_38.png'});
+    }
+}
+
+function save_options() {
+    var obj = {enabled: toggle.checked};
+    updateIcon(toggle.checked);
+    storage.set(obj);
+}
+
+function restore_options() {
+    storage.get('enabled', function (result) {
+        console.log(result);
+        toggle.checked = result.enabled;
+    });
+}
+
+
+// document.addEventListener('DOMContentLoaded', restore_options);
+// document.getElementById('save').addEventListener('click', save_options);
